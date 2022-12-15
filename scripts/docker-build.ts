a13465d2e221c80601e24d6e108091c0c06a41d3
dingdangdog <dddogx@qq.com>
2022-12-15 16:19:03 +0800

    feat: 增加docker一键构建镜像脚本

---
+++ scripts/docker-build.ts
@@ -0,0 +1,26 @@
+const docker_build = () => {
+  const fs = require("fs");
+  const packageInfo = fs.readFileSync("package.json", { encoding: "utf-8" });
+  const pack = JSON.parse(packageInfo);
+  const version = pack.version;
+
+  const dockerfile = fs.readFileSync("Dockerfile_lock", { encoding: "utf-8" });
+  const realDockerfile = dockerfile.replace("{{version}}", version);
+  fs.writeFileSync("Dockerfile", realDockerfile, { encoding: "utf-8" });
+
+  // 添加脚本执行器
+  const { execSync } = require("child_process");
+
+  execSync(
+    `docker build -t dingdangdog/cashbook:${version} . && docker build -t dingdangdog/cashbook:latest .`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
+
+}
+docker_build();
