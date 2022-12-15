bfbb9b3394008b0e0e9c7b8832f566d0cd8349b1
dingdangdog <dddogx@qq.com>
2022-12-15 11:31:41 +0800

    docs: 更新文档

--- docs/ref/changelog.md
+++ docs/ref/changelog.md
@@ -4,10 +4,25 @@ icon: update
 ---
 
 
+### [0.0.8](https://github.com/DingDangDog/cashbook/compare/v0.0.7...v0.0.8) (2022-12-15)
 
-All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### 🚙 Styles | 风格
+
+* 文档图标修改 ([f7253e0](https://github.com/DingDangDog/cashbook/commit/f7253e0032051d86fa5b68a4e696a7507b9a6fec))
+
+### [0.0.7](https://github.com/DingDangDog/cashbook/compare/v0.0.6...v0.0.7) (2022-12-15)
+
+
+### 📚 Documentation | 文档
+
+* CHANGELOG文档自动更新 ([baaea47](https://github.com/DingDangDog/cashbook/commit/baaea47fd48a0dbd26216f74685e6f973e26b95c))
+
+
+### ⛽ Continuous Integration | CI 配置
+
+* 增加standard-version配置文件 ([3aacd06](https://github.com/DingDangDog/cashbook/commit/3aacd0639d4745f503b88b52200c522550173181))
 
 ### [0.0.6](https://github.com/DingDangDog/cashbook/compare/v0.0.5...v0.0.6) (2022-12-14)
 
 ### [0.0.5](https://github.com/DingDangDog/cashbook/compare/v0.0.4...v0.0.5) (2022-12-14)
-
--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,29 +1,25 @@
 const insertLog = () => {
   const fs = require("fs");
-  let log = fs.readFileSync("CHANGELOG.md", { encoding: "utf-8" });
-  // 本地配置修改
-  log = log.replace("https://ddd/", "https://github.com/");
-  fs.writeFileSync("CHANGELOG.md", log, { encoding: "utf-8" });
-
+  const log = fs.readFileSync("CHANGELOG.md", { encoding: "utf-8" });
   const newLog =
     "---\ntitle: 更新日志\nicon: update\n---\n\n" +
     log.replace("# Changelog", "");
 
   fs.writeFileSync("docs/ref/changelog.md", newLog, { encoding: "utf-8" });
 
-  // 文档版本标识文件，暂不需要
-  // let version = fs.readFileSync("doc-version", { encoding: "utf-8" });
-  // version = version.split("\n")[0].trim();
-  // const arr = version.split(".");
-  // const sub = arr.pop();
-  // arr.push(String(parseInt(sub) + 1));
-  // const newVersion = arr.join(".");
-  // fs.writeFileSync("doc-version", newVersion, { encoding: "utf-8" });
+  // 更新文档版本更新标识文件
+  let version = fs.readFileSync("doc-version", { encoding: "utf-8" });
+  version = version.split("\n")[0].trim();
+  const arr = version.split(".");
+  const sub = arr.pop();
+  arr.push(String(parseInt(sub) + 1));
+  const newVersion = arr.join(".");
+  fs.writeFileSync("doc-version", newVersion, { encoding: "utf-8" });
 
   // 添加脚本执行器并应用
   const { execSync } = require("child_process");
   execSync(
-    `git add . && git commit -m \"docs: CHANGELOG文档自动更新\"`,
+    `git add . && git commit -m \"docs: 更新文档\" && git tag doc-${newVersion} && git push --follow-tags origin main && git push --tags`,
     (err, stdout, stderr) => {
       if (err) {
         console.log(err);
