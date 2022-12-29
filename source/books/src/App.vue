3463a18593aa4a0211eed2db9c536cc9bd471acc
dingdangdog <dddogx@qq.com>
2022-12-29 10:55:51 +0800

    fix: 友链没发现问题，可能是网络原因导致？暂时关闭吧 close #15

--- source/books/src/App.vue
+++ source/books/src/App.vue
@@ -54,7 +54,6 @@
         </div>
       </el-main>
 
-
       <el-footer>
         <p style="margin-top: 0px; margin-bottom: 0px;">Powered by <a href="https://github.com/DingDangDog/cashbook">DingDangDog/cashbook_v{{serverInfo.version}}</a></p>
         <p style="margin-top: 0px; margin-bottom: 0px;">友链：<a href="https://oldmoon.top/about">oldmoon.top</a></p>
